335e09f787d82605e2e8d8853833b44804181bec
Mauvonte Roberts <moeroberts183@gmail.com>
2022-11-01 19:17:47 -0400

    fixed analysis main txt

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,17 +2,17 @@
 
 import os
 import csv
-csvpath = os.path.join('..','Resources', 'election_data.csv')
+csvpath = os.path.join('Resources', 'election_data.csv')
 
 
 
 # Initialize the 'Total Vote' count and the 'Winning' count. 
 total_votes = 0
-winning_count = 0
+winner_count = 0
 # Initialize the list of candidates. 
-candidates_list = []
+candidates = []
 # Initialize the dictionary of candidates and their votes. 
-candidate_votes = {}
+candid_votes = {}
 
 
 
@@ -46,46 +46,46 @@ with open(csvpath) as csv_file:
 # Iterating through the rows in the election data file. 
     for row in csvreader:
         # Identifying the rows of candidates in the eletion data file. 
-        candidate_name = row[2]
+        candid_name = row[2]
         # Incrementing the total vote count for each row in the election data file. 
         total_votes = total_votes + 1
         # Adding a new candidate name to a list for every row in the election data file. 
-        if candidate_name not in candidates_list:
-            candidates_list.append(candidate_name)
+        if candid_name not in candidates:
+            candidates.append(candid_name)
             # Initializing the vote count for each new candidate name. 
-            candidate_votes[candidate_name] = 0
+            candid_votes[candid_name] = 0
         # Adding to the vote count of every candidate name if similar. 
-        candidate_votes[candidate_name] += 1
+        candid_votes[candid_name] += 1
 # Writing the first part of the results of the analysis. 
-results_1 = str(f"Election Results\n"
+first_result = str(f"Election Results\n"
                     f"------------------\n"
                     f"Total Votes: {total_votes}\n"
                     f"------------------"
                     )
 # Initializing the add-on to the second part of the analysis results. 
-all_results_2 = ""
+results = ""
 # Initiating a for loop for the 'Candidate Votes' dictionary. 
-for candidate_name, votes in candidate_votes.items():
+for candid_name, votes in candid_votes.items():
     # Calculating the percentage of votes cast for each candidate. 
     vote_percentage = votes / total_votes
     # Formatting the vote percentage of each candidate to have three decimal places and a percentage sign. 
     vote_percentage_formatted = "{:.3%}".format(vote_percentage)
     # Writing the second part of the analysis results. 
-    results_2 = str(f"{candidate_name}: {vote_percentage_formatted} ({votes})")
+    sec_results = str(f"{candid_name}: {vote_percentage_formatted} ({votes})")
     # Adding a new line after each iteration of the second part of the analysis results. 
-    all_results_2 += '\n'
+    results += '\n'
     # Adding the second part of the analysis results to the add-on for the second part. 
-    all_results_2 += results_2
+    results += sec_results
     # Calculating the winning candidate. 
-    if votes > winning_count:
-        winning_count = votes
-        winner = candidate_name
+    if votes > winner_count:
+        winner_count = votes
+        winner = candid_name
 # Writing the third part of the analysis results. 
-results_3 = str(f"---------------------\n"
+third_result = str(f"---------------------\n"
                     f"Winner: {winner}\n"
                     f"---------------------")
 # Writing the final part of the analysis results which sums up the other three parts including the add-on. 
-total_results = str(f"{results_1} \n {all_results_2} \n {results_3}")
+total_results = str(f"{first_result} \n {results} \n {third_result}")
 # Saves the analysis results to a text file. 
 output_path = os.path.join('analysis', 'main.txt')
 with open (output_path, 'w') as textfile:
@@ -125,9 +125,9 @@ with open (output_path, 'w') as textfile:
 
 
 
-print(f"{results_1}\n")
-print(f"{results_2}\n")
-print(f"{results_3}\n")
+print(f"{first_result}\n")
+print(f"{sec_results}\n")
+print(f"{third_result}\n")
 
 print(f"{total_results}\n")
 
